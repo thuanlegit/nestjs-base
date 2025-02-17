184e4ac0625da4a38b1322b5e4ee163b4299923d
thuan.le <thuan.le@enouvo.com>
2025-02-17 17:22:58 +0700

    chore: add pagination dto & swagger decorator

---
+++ src/common/decorators/api-ok-paginated-response.decorator.ts
@@ -0,0 +1,24 @@
+import { applyDecorators, Type } from '@nestjs/common';
+import { ApiExtraModels, ApiOkResponse, getSchemaPath } from '@nestjs/swagger';
+
+import { PaginatedResponse } from '../dto';
+
+export const ApiOkPaginatedResponse = <DataDto extends Type<unknown>>(dataDto: DataDto) =>
+  applyDecorators(
+    ApiExtraModels(PaginatedResponse, dataDto),
+    ApiOkResponse({
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(PaginatedResponse) },
+          {
+            properties: {
+              data: {
+                type: 'array',
+                items: { $ref: getSchemaPath(dataDto) },
+              },
+            },
+          },
+        ],
+      },
+    }),
+  );
---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsOptional, Max, Min } from 'class-validator';
+
+export class PaginationQueryDto {
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  page: number = 1;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  @IsOptional()
+  limit: number = 20;
+}
+
+export class PaginatedResponse<T> {
+  data!: T[];
+  total!: number;
+  page!: number;
+  limit!: number;
+}
